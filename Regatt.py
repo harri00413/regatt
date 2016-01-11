171ee53197cfc0397ad9bacb557a97f2242066c9
Frank Harland <fharland@gmail.com>
2016-01-11 16:54:02 +0100

    Updated

---
+++ Regatt.py
@@ -0,0 +1,50 @@
+# UI deel van Regatt using standard UI stuff
+
+from time import time, strftime, gmtime
+from console import hud_alert
+from calc_time import berekendetijd
+
+
+# Some vars
+tStartTijd = 0.0
+tStopTijd = 0.0
+tTimer = 0.0
+bRunning = False
+
+def button_tapped(sender):
+  global bRunning
+  global tTimer
+  global tStartTijd
+  global tStopTijd
+  t = sender.title
+  if t == "Start":
+    sender.title = "Stop"
+    btnStartStop.tint_color = 1.00, 0.00, 0.00
+    tStartTijd = time()
+    swLock.enabled = True
+    bRunning = True
+
+  else:
+    if swLock.value:
+      hud_alert('Stop button is locked. Unlock to use stop.')
+      else:
+        tStoptijd = time()
+        tTimer = strftime("%H:%M:%S", gmtime(tStoptijd - tStartTijd))
+        sender.title = "Start"
+        btnStartStop.tint_color = 0.00, 1.00, 0.00
+        labTijd.text = str(tTimer)
+
+
+# Main program
+v = ui.load_view('TestUI')
+labTijd = v['lbltijd']
+labTijd.text = "00:00:00"
+swLock = v['swLock']
+btnStartStop = v['btnStart']
+
+if ui.get_screen_size()[1] >= 768:
+  # iPad & larger
+  v.present('popover')
+else:
+  # iPhone
+  v.present(orientations=['portrait'])
--- calc_time.py
+++ calc_time.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 # Berekening gezeilde tijd met omrekening met behulp van de SW waarde
 # Inputs:
-# Gezeilde uren, minuten en seconden in hh:mm:ss
+# Gezeilde uren, minuten en seconden in uren, minuten en seconden en SW
 # SW in float
 # Output: Berekende tijd als string
 # BerekendeTijd = GezeildeTijd * 100 / SW
@@ -13,7 +13,7 @@ import datetime
 # noinspection PyShadowingNames
 def berekendetijd(gezeildeuren, gezeildeminuten, gezeildeseconden, sw):
     tinsec = get_sec(gezeildeuren, gezeildeminuten, gezeildeseconden) * 100 / sw
-    return datetime.timedelta(seconds=tinsec)
+    return datetime.timedelta(seconds=round(tinsec), microseconds=0)
 
 
 def get_sec(u, m, s):
@@ -21,9 +21,9 @@ def get_sec(u, m, s):
 
 
 # main()
-igezeildeuren = int(input("Geef gezeilde uren: "))
-igezeildeminuten = int(input("Geef gezeilde minuten: "))
-igezeildeseconden = int(input("Geef gezeilde seconden: "))
-sw = int(input("Geef SW: "))
+# igezeildeuren = int(input("Geef gezeilde uren: "))
+# igezeildeminuten = int(input("Geef gezeilde minuten: "))
+# igezeildeseconden = int(input("Geef gezeilde seconden: "))
+# sw = int(input("Geef SW: "))
 
-print("Berekende tijd: " + str(berekendetijd(igezeildeuren, igezeildeminuten, igezeildeseconden, sw)))
+# print("Berekende tijd: " + str(berekendetijd(igezeildeuren, igezeildeminuten, igezeildeseconden, sw)))
